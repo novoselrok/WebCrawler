f216e1acff9d7de08ee10b2c1c489a9a280f569b
Rok Novosel <novosel.rok@gmail.com>
2014-09-25 14:02:06 +0200

    Added some try/catches

--- src/main/java/com/redditprog/webcrawler/App.java
+++ src/main/java/com/redditprog/webcrawler/App.java
@@ -1,5 +1,6 @@
 package com.redditprog.webcrawler;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,32 +34,47 @@ public class App {
 		try {
 			page = Jsoup.connect("http://www.reddit.com/r/" + sub+ "/top/?sort=top&t=month").get();
 			
+			//Selecting all the elements with HTML class "title", that have nested inside <a href="..">..</a> tags
+			//that end with jpg or png
 			Elements images = page.select(".title").select("a[href$=jpg], a[href$=png]");
 
 			for (Element link : images) {
-				if (i == num_pics)
-					break;
-				// System.out.println(i + " " + link.attr("href"));
+				if (i == num_pics) break;
+				
+				//Saving the url of the picture
 				URL addr = new URL(link.attr("href"));
 				InputStream in = addr.openStream();
-				OutputStream op;
+				OutputStream op = null;
+				String[] tab = link.attr("href").split("/");
 				//TODO: If the specified path doesn't exist try and create it
-				if (link.attr("href").endsWith("jpg")) {
-					op = new FileOutputStream(dir + i + ".jpg");
-				} else {
-					op = null;
-					System.out.println("Sorry, no dice.. yet");
+				try{
+					if (link.attr("href").endsWith("jpg")) {
+						op = new FileOutputStream(dir + tab[tab.length-1]);
+					} else if(link.attr("href").endsWith("png")){
+						op = new FileOutputStream(dir + tab[tab.length-1]);
+					} else {
+						System.out.println("Sorry, no dice.. yet");
+					}
+				}catch(FileNotFoundException e){
+					System.out.println("You have entered an invalid path. Try again.");
+					System.exit(-1);
 				}
+				//Saving the picture to the file
 				byte[] b = new byte[20480];
-				int length;
-				while ((length = in.read(b)) != -1) {
-					// writing it to a file
-					op.write(b, 0, length);
+				try{
+					int length;
+					while ((length = in.read(b)) != -1) {
+						op.write(b, 0, length);
+					}
+				}catch(IOException e){
+					System.out.println("An error occured while saving the picture.");
 				}
 				in.close();
-				op.close();
-				i++;
+				op.close();		
+				
 				System.out.println("Download complete: " + link.attr("href"));
+				System.out.println("File has been saved in: " + dir + tab[tab.length-1]);
+				i++;
 			}
 
 		} catch (IOException e) {
