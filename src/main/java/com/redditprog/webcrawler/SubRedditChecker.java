92dbe9f4810a7269c3ff64851db2ca0588c66da4
Daytron <ryangilera@gmail.com>
2014-09-26 20:02:23 +0100

    Fix link checker bug

--- src/main/java/com/redditprog/webcrawler/SubRedditChecker.java
+++ src/main/java/com/redditprog/webcrawler/SubRedditChecker.java
@@ -5,6 +5,7 @@
  */
 package com.redditprog.webcrawler;
 
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,28 +14,40 @@ import java.net.URL;
  * @author ryan
  */
 public class SubRedditChecker {
+
     public static boolean verifySubReddit(String sub) {
-        
+
         try {
             // set the full url of the user input subreddit
-            final URL url =  new URL("http://reddit.com/r/" + sub);
-            
+            final URL url = new URL("http://reddit.com/r/" + sub);
+
             // create a new connection
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-            
+
             // disable any redirect, since reddit automatically use search if
             // subreddit does not exist
-            huc.setInstanceFollowRedirects(false);
-            
             // Uses HEAD as request for faster link check
             huc.setRequestMethod("HEAD");
             
-            return (huc.getResponseCode() == HttpURLConnection.HTTP_OK);
+            // connects to the http
+            huc.connect();
+
+            // Possible solution (do not delete)
+            //System.out.println("connected url: " + huc.getURL());
+
+            // Extract the redirect url in string
+            InputStream is = huc.getInputStream();
+            String redirectURL = huc.getURL().getPath();
+            is.close();
+            
+            // checks if it is a redirect and return boolean value
+            return redirectURL.contains("/r/");
+
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
-        
+
     }
-    
+
 }
