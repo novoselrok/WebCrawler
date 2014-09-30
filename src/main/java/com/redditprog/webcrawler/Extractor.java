c4faf4f7dd33b5cc0f797963a4471a5a3bdd1973
Rok Novosel <novosel.rok@gmail.com>
2014-09-30 14:38:52 +0200

    Added imgur album api, general fixes

--- src/main/java/com/redditprog/webcrawler/Extractor.java
+++ src/main/java/com/redditprog/webcrawler/Extractor.java
@@ -1,11 +1,10 @@
-
 package com.redditprog.webcrawler;
-
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,13 +15,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
-
 
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,296 +34,300 @@ import org.json.JSONObject;
  */
 public class Extractor {
 
-    // Instance variables
-    private final int num_pics;
-    private final String sub;
-    private final String dir;
-    private final String range;
-    private final Scanner scanner;
+	// Instance variables
+	private final int num_pics;
+	private final String sub;
+	private final String dir;
+	private final String type_of_links;
+	private final String top_time;
+	private final Scanner scanner;
 
-    /**
-     * The class constructor with the following parameters:
-     *
-     * @param sub SubReddit name
-     * @param num Number of pictures to extract
-     * @param dir Directory path to save photos
-     * @param top_time Range of Date/Time for the subreddit
-     * @param scanner Scanner object pass from Launcher
-     */
-    public Extractor(String sub, int num, String dir,
-            String top_time, Scanner scanner) {
-        this.sub = sub;
-        this.num_pics = num;
-        this.dir = dir;
-        this.range = top_time;
-        this.scanner = scanner;
+	/**
+	 * The class constructor with the following parameters:
+	 *
+	 * @param sub
+	 *            SubReddit name
+	 * @param num
+	 *            Number of pictures to extract
+	 * @param dir
+	 *            Directory path to save photos
+	 * @param top_time
+	 *            Range of Date/Time for the subreddit
+	 * @param scanner
+	 *            Scanner object pass from Launcher
+	 */
+	public Extractor(String sub, int num, String dir, String type_of_links,
+			String top_time, Scanner scanner) {
+		this.sub = sub;
+		this.num_pics = num;
+		this.dir = dir;
+		this.type_of_links = type_of_links;
+		this.top_time = top_time;
+		this.scanner = scanner;
 
-    }
+	}
 
-    public void beginExtract() {
+	public void beginExtract() {
 
-        // set the full url of the user input subreddit
-        final URL urlSub, urlJson;
+		// set the full url of the user input subreddit
+		URL urlJson;
+		String json_url;
+		int numDownloads = 0;
+		try {
+			// urlSub = new URL("http://reddit.com/r/" + this.sub);
+			// http://www.reddit.com/r/Gunners/top/.json?sort=top&t=month
+			if (type_of_links.equals("top")) {
+				json_url = "http://www.reddit.com/r/" + this.sub + "/"
+						+ this.type_of_links + "/.json?sort=top&t="
+						+ this.top_time;
+			} else {
+				json_url = "http://www.reddit.com/r/" + this.sub + "/"
+						+ this.type_of_links + "/.json";
 
-        try {
-            urlSub = new URL("http://reddit.com/r/" + this.sub);
-            urlJson = new URL("http://www.reddit.com/r/" + this.sub + "/"
-                    + this.range + ".json");
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        }
+			}
+			urlJson = new URL(json_url);
+		} catch (MalformedURLException ex) {
+			Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null,
+					ex);
+			return;
+		}
 
-        String jsonString = this.extractJsonFromUrl(urlJson);
+		JSONObject obj;
+		JSONArray childArray;
+		int count = 25;
 
-        JSONObject obj;
-        JSONArray childArray;
-        int numDownloads = 0;
+		while (true) {
 
-        try {
-            obj = new JSONObject(jsonString);
+			String jsonString = this.extractJsonFromUrl(urlJson);
 
-            childArray = obj.getJSONObject("data").getJSONArray("children");
+			try {
+				obj = new JSONObject(jsonString);
+				String after = obj.getJSONObject("data").getString("after");
+				childArray = obj.getJSONObject("data").getJSONArray("children");
 
-            mainloop:
-            for (int i = 0; i < childArray.length(); i++) {
-                String urlString = "";
-                
-                if (childArray.getJSONObject(i)
-                        .getJSONObject("data").has("url")) {
-                    urlString = childArray.getJSONObject(i)
-                        .getJSONObject("data").getString("url");
-                } else  {
-                    // Gilded range change the Data's url child to link_url
-                    if (childArray.getJSONObject(i)
-                        .getJSONObject("data").has("link_url")) {
-                        urlString = childArray.getJSONObject(i)
-                        .getJSONObject("data").getString("link_url");
-                    }
-                }
-                
-                
-                if (urlString.contains("imgur")) {
-                    URL url = new URL(urlString);
+				// mainloop:
+				for (int i = 0; i < childArray.length(); i++) {
+					String urlString = "";
 
-                    if (urlString.substring(urlString.lastIndexOf("/"))
-                            .contains(".")) {
-                        numDownloads = this.extractImgurSingle(numDownloads, url);
+					if (childArray.getJSONObject(i).getJSONObject("data")
+							.has("url")) {
+						urlString = childArray.getJSONObject(i)
+								.getJSONObject("data").getString("url");
+					} else {
+						// Gilded range change the Data's url child to link_url
+						if (childArray.getJSONObject(i).getJSONObject("data")
+								.has("link_url")) {
+							urlString = childArray.getJSONObject(i)
+									.getJSONObject("data")
+									.getString("link_url");
+						}
+					}
 
-                    } else {
-                        numDownloads = this.extractImgurAlbum(numDownloads, url);
-                    }
+					URL url;
+					if (urlString.contains("imgur.com/a/")) {
+						url = new URL(urlString);
+						numDownloads = this
+								.extractImgurAlbum(numDownloads, url);
+					} else {
 
-                    if (numDownloads == this.num_pics) {
-                        break;
-                    }
+						if (urlString.contains("i.imgur.com")) {
+							url = new URL(urlString);
+						} else {
+							urlString = urlString.replace("imgur", "i.imgur");
+							System.out.println(urlString);
+							url = new URL(urlString + ".png");
+						}
 
-                }
+						numDownloads = this.extractImgurSingle(numDownloads,
+								url);
+					}
+					if (numDownloads >= this.num_pics) {
+						break;
+					}
 
-            }
-        } catch (JSONException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        }
+				}
 
-        if (numDownloads > 0) {
-            if (numDownloads < this.num_pics) {
-                System.out.println("=================================");
-                System.out.println("There are no more pictures found.");
-                System.out.println("=================================");
-            }
-            this.askUserToOpenFolder();
-        } else {
-            System.out.println("==================");
-            System.out.println("No Pictures found!");
-            System.out.println("==================");
-        }
+				if (type_of_links.equals("top")) {
+					json_url += "&count=" + count + "&after=" + after;
+				} else {
+					json_url += "?count=" + count + "&after=" + after;
+				}
+				System.out.println("JSONURL= " + json_url + "NUMDOWNLOADS= "
+						+ numDownloads);
+				urlJson = new URL(json_url);
+				count += 25;
+			} catch (JSONException ex) {
+				Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE,
+						null, ex);
+			} catch (MalformedURLException ex) {
+				Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE,
+						null, ex);
+			}
 
-    }
+			if (this.num_pics <= numDownloads) {
+				this.askUserToOpenFolder();
+				break;
+			}
 
-    private int extractImgurSingle(int numDownloads, URL url) {
+		}
+	}
 
-        String fileName = url.getFile();
-        // + 1 because this.dir already got "/" as the last character
-        String destName = this.dir + fileName.substring(fileName.lastIndexOf("/")+1);
+	private int extractImgurSingle(int numDownloads, URL url) {
 
-        InputStream is;
-        OutputStream os;
-        try {
-            is = url.openStream();
-            os = new FileOutputStream(destName);
-            byte[] b = new byte[2048];
-            int length;
+		String fileName = url.getFile();
+		// + 1 because this.dir already got "/" as the last character
+		String destName = this.dir
+				+ fileName.substring(fileName.lastIndexOf("/") + 1);
 
-            while ((length = is.read(b)) != -1) {
-                os.write(b, 0, length);
-            }
+		InputStream is;
+		OutputStream os;
+		try {
+			is = url.openStream();
+			os = new FileOutputStream(destName);
+			byte[] b = new byte[2048];
+			int length;
 
-            is.close();
-            os.close();
-            
-            this.printDownloadCompleted(numDownloads, destName);
-            return numDownloads + 1;
-        } catch (IOException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-            return numDownloads;
-        }
+			while ((length = is.read(b)) != -1) {
+				os.write(b, 0, length);
+			}
 
-        
-    }
+			is.close();
+			os.close();
 
-    private int extractImgurAlbum(int numDownloads, URL url) {
-        HtmlPage imgurPage;
-        HtmlImage image;
+			this.printDownloadCompleted(numDownloads, destName);
+			return numDownloads + 1;
+		} catch (IOException ex) {
+			Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null,
+					ex);
+			return numDownloads;
+		}
 
-        // create a new connection
-        HttpURLConnection huc;
-        try {
-            huc = (HttpURLConnection) url.openConnection();
-            // connects to the http
-            huc.connect();
-            WebClient aWebClient = new WebClient(BrowserVersion.CHROME);
-            aWebClient.getOptions().setJavaScriptEnabled(false);
-            imgurPage = aWebClient.getPage(url);
+	}
 
-            final List<?> images = imgurPage.getByXPath("//img");
+	private int extractImgurAlbum(int numDownloads, URL url) {
+		String Client_ID = "6e4aad85ecee137";
+		String Client_Secret = "9f6c50ff4fca8f076a83db05fc4799e84e4e8c3b";
+		String[] url_s = url.toString().split("/");
 
-            int numTitle = 1;
-            String imageSrc;
+		JSONObject obj;
+		JSONArray images_array;
+		try {
 
-            for (Object imageObject : images) {
-                // Typecast to HtmlImage
-                image = (HtmlImage) imageObject;
+			URL jsonUrl = new URL("https://api.imgur.com/3/album/"
+					+ url_s[url_s.length - 1]);
 
-                imageSrc = image.getAttribute("src");
+			HttpURLConnection conn = (HttpURLConnection) jsonUrl
+					.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Authorization", "Client-ID " + Client_ID);
 
-                // If the object found is not jpeg, jpg or png filetype, 
-                // move to next object
-                if (!imageSrc.contains(".")) {
-                    continue;
-                }
-                if (!((imageSrc.contains(".jpeg"))
-                        || (imageSrc.contains(".jpg"))
-                        || (imageSrc.contains(".png")))) {
-                    continue;
-                }
+			BufferedReader bin = null;
+			bin = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			StringBuilder jsonString = new StringBuilder();
+			// below will print out bin
+			String line;
+			while ((line = bin.readLine()) != null)
+				jsonString.append(line);
 
-                // Bypass any non-user submitted photo (e.g. website layout image)
-                if (image.getHeight() < 200){
-                    continue;
-                }
+			bin.close();
 
-                // Create new arbritrary file to save photo later
-                File file;
-                try {
-                    file = new File(this.dir
-                            + imageSrc.substring(imageSrc.lastIndexOf("/")+1));
+			obj = new JSONObject(jsonString.toString());
+			images_array = obj.getJSONObject("data").getJSONArray("images");
 
-                    while (file.exists()) {
-                        numTitle += 1;
-                        file = new File(this.dir
-                            + numTitle + imageSrc.substring(imageSrc.lastIndexOf(".")));
-                    }
+			for (int i = 0; i < images_array.length(); i++) {
+				numDownloads = extractImgurSingle(numDownloads, new URL(
+						images_array.getJSONObject(i).getString("link")));
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-                    image.saveAs(file);
+		return numDownloads;
+	}
 
-                    numTitle += 1;
+	/*
+	 * private void filterPhoto() {
+	 * 
+	 * }
+	 */
+	private void printDownloadCompleted(int num, String path) {
+		System.out.println("==================");
+		System.out.println("Download #" + (num + 1) + " complete:");
+		System.out.println("Name of the file: " + path);
+	}
 
-                    this.printDownloadCompleted(numDownloads, file.getPath());
-                   
+	private String extractJsonFromUrl(URL url) {
+		BufferedReader reader = null;
+		try {
 
-                    if (this.num_pics == numDownloads) {
-                        break;
-                    }
-                    numDownloads += 1;
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1) {
+				buffer.append(chars, 0, read);
+			}
 
-                    if (numDownloads == this.num_pics) {
-                        break;
-                    }
-                } catch (IOException ex) {
-                    Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-                    System.exit(1);
-                }
-                numTitle += 1;
-            }
+			return buffer.toString();
 
-            aWebClient.closeAllWindows();
-        } catch (IOException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-            System.exit(1);
-        }
+		} catch (IOException ex) {
+			Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null,
+					ex);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException ex) {
+					Logger.getLogger(Extractor.class.getName()).log(
+							Level.SEVERE, null, ex);
+				}
+			}
+		}
 
-        return numDownloads;
-    }
-    
-    private void filterPhoto() {
-        
-    }
+		return "";
+	}
 
-    private void printDownloadCompleted(int num, String path) {
-        System.out.println("==================");
-        System.out.println("Download #" + (num + 1) + " complete:");
-        System.out.println("Name of the file: " + path);
-    }
+	private void openFolder() {
+		try {
+			Desktop.getDesktop().open(new File(this.dir));
+		} catch (IOException e) {
+			System.out
+					.println("Ooops, looks like this folder doesn't exist :(");
+			e.printStackTrace();
+		}
+	}
 
-    private String extractJsonFromUrl(URL url) {
-        BufferedReader reader = null;
-        try {
-
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1) {
-                buffer.append(chars, 0, read);
-            }
-
-            return buffer.toString();
-
-        } catch (IOException ex) {
-            Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(Extractor.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
-
-        return "";
-    }
-
-    private void openFolder() {
-        try {
-            Desktop.getDesktop().open(new File(this.dir));
-        } catch (IOException e) {
-            System.out.println("Ooops, looks like this folder doesn't exist :(");
-            e.printStackTrace();
-        }
-    }
-
-    private void askUserToOpenFolder() {
-        System.out.println("==================");
-        System.out.println("Download finished!");
-        System.out.println("==================");
-        System.out.println("Do you want to open " + this.dir + "\nin your File Explorer? (y/n)");
-        boolean isSelected = false;
-        while (!isSelected) {
-            String openFolder = this.scanner.next();
-            if (openFolder.equalsIgnoreCase("y") || openFolder.equalsIgnoreCase("yes")) {
-                this.openFolder();
-                isSelected = true;
-            } else if (openFolder.equalsIgnoreCase("n") || openFolder.equalsIgnoreCase("no")) {
-                isSelected = true;
-            } else {
-                System.out.println("Enter y or n");
-            }
-        }
-    }
+	private void askUserToOpenFolder() {
+		System.out.println("==================");
+		System.out.println("Download finished!");
+		System.out.println("==================");
+		System.out.println("Do you want to open " + this.dir
+				+ "\nin your File Explorer? (y/n)");
+		boolean isSelected = false;
+		while (!isSelected) {
+			String openFolder = this.scanner.next();
+			if (openFolder.equalsIgnoreCase("y")
+					|| openFolder.equalsIgnoreCase("yes")) {
+				this.openFolder();
+				isSelected = true;
+			} else if (openFolder.equalsIgnoreCase("n")
+					|| openFolder.equalsIgnoreCase("no")) {
+				isSelected = true;
+			} else {
+				System.out.println("Enter y or n");
+			}
+		}
+	}
 
 }
