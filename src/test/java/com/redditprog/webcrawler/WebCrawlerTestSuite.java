fb0ba380e91ea4b9bbe54608f0487052eb0eec78
Daytron <ryangilera@gmail.com>
2014-10-20 17:38:31 +0100

    Add test suite and remove App class test

---
+++ src/test/java/com/redditprog/webcrawler/WebCrawlerTestSuite.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditprog.webcrawler;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Application Test Suite
+ * @author ryan
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    LauncherTest.class
+})
+public class WebCrawlerTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
